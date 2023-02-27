9e3d5f903b1d04fef02f409eba64e02616790985
Babyshark <Rulic1137@gmail.com>
2023-02-27 14:14:32 +0700

    return to original

--- face-detect.py
+++ face-detect.py
@@ -1,21 +1,21 @@
 import cv2 as cv
 
-face_model = cv.CascadeClassifier('face-detect-model1.xml')
-cap = cv.VideoCapture(0) # open the default camera
+face_model = cv.CascadeClassifier('face-detect-model.xml')
+cap = cv.VideoCapture(0) 
 
 while True:
-    ret, frame = cap.read() # read a frame from the camera
-    if not ret: # if there was an error reading the frame
+    ret, frame = cap.read() 
+    if not ret: 
         break
     
     gray_scale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = face_model.detect1MultiScale(gray_scale)
+    faces = face_model.detectMultiScale(gray_scale)
     
     for (x, y, w, h) in faces:
         cv.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
         
     cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'): # exit on pressing 'q' key
+    if cv.waitKey(1) == ord('q'):
         break
 
 cap.release()
